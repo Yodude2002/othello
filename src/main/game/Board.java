4fd35cb58a0622350c4cb282d65abba6ace06f24
TheSpeedyMouse <drewheermance384@gmail.com>
2019-06-06 14:17:24 -0400

    Created a findPossibleMoves method as well as added setColor and getColor to the Player interface
    Signed-off-by: TheSpeedyMouse <drewheermance384@gmail.com>

--- src/main/game/Board.java
+++ src/main/game/Board.java
@@ -65,20 +65,6 @@ public class Board {
     }
 
     /**
-     * When flipping disks on a direction, a direction should no longer be updated if a tile reached has no disk
-     * or a disk of the color from the originating tile in the direction.
-     * @param r Row of current tile in the direction
-     * @param c Column of current tile in the direction
-     * @param color Color of the tile at the begining of a direction. In all 8 directions from a tile,
-     *              the tile in the middle is defined as the tile at the begining of a direction.
-     *              True is white, false is black.
-     * @return True if a direction should no longer be updated, false if continue updating tiles.
-     */
-    private boolean shouldStopUpdatingDirection(byte r, byte c, boolean color) {
-        return ((!currentBoard.getHasTile()[r][c]) || (currentBoard.getColor()[r][c] == color));
-    }
-
-    /**
      * Travels verically from (r,c) and flips tiles accoridng to Othello Rules:
      * If a tile is the opposite color of color, than flip.
      * Stop flipping tiles if a tile does not have a disk
@@ -87,73 +73,99 @@ public class Board {
      * @param color
      */
     private void flipUp(byte r, byte c, boolean color) {
-        for (byte row = (byte) (r-1); row >= 0; row--) {
-            if (shouldStopUpdatingDirection(row,c, color)) {
-
-            }
+        byte row = (byte) (r-1);
+        while (currentBoard.getColor()[row][c] == color) {
+            if (row < 0 || !currentBoard.getHasTile()[row][c])
                 return;
-            currentBoard.getColor()[row][c] = color;
+            row--;
         }
+        while (++row < r)
+            currentBoard.getColor()[row][c] = color;
     }
 
     private void flipUpRight(byte r, byte c, boolean color) {
-        for (byte row = (byte) (r-1); row >= 0; row--) {
-            c++;
-            if (c >= currentBoard.getColor()[r].length || shouldStopUpdatingDirection(row,c,color))
+        byte row = (byte) (r-1);
+        byte col = (byte) (c+1);
+        while (currentBoard.getColor()[row][col] == color) {
+            if (row < 0 || col >= currentBoard.getColor()[row].length || !currentBoard.getHasTile()[row][col])
                 return;
-            currentBoard.getColor()[row][c] = color;
+            row--;
+            col++;
         }
+        while (++row < r && --col > c)
+            currentBoard.getColor()[row][col] = color;
     }
 
     private void flipRight(byte r, byte c, boolean color) {
-        for (byte col = (byte) (c+1); col < currentBoard.getColor()[r].length; col++) {
-            if (shouldStopUpdatingDirection(r,col, color))
+        byte col = (byte) (c+1);
+        while (currentBoard.getColor()[r][col] == color) {
+            if (col >= currentBoard.getColor()[r].length || !currentBoard.getHasTile()[r][col])
                 return;
-            currentBoard.getColor()[r][col] = color;
+            col++;
         }
+        while (--col > c)
+            currentBoard.getColor()[r][col] = color;
     }
 
     private void flipDownRight(byte r, byte c, boolean color) {
-        for (byte col = (byte)(c+1); col < currentBoard.getColor()[r].length; col++) {
-            r++;
-            if (r >= currentBoard.getColor().length || shouldStopUpdatingDirection(r,col,color))
+        byte row = (byte) (r+1);
+        byte col = (byte) (c+1);
+        while (currentBoard.getColor()[row][col] == color) {
+            if (row >= currentBoard.getColor().length || col >= currentBoard.getColor()[row].length || !currentBoard.getHasTile()[row][col])
                 return;
-            currentBoard.getColor()[r][col] = color;
+            row++;
+            col++;
         }
+        while (--row > r && --col > c)
+            currentBoard.getColor()[row][col] = color;
     }
 
     private void flipDown(byte r, byte c, boolean color) {
-        for (byte row = (byte) (r+1); row < currentBoard.getColor().length; row++) {
-            if (shouldStopUpdatingDirection(row,c,color))
+        byte row = (byte) (r+1);
+        while (currentBoard.getColor()[row][c] == color) {
+            if (row >= currentBoard.getColor().length || !currentBoard.getHasTile()[row][c])
                 return;
-            currentBoard.getColor()[row][c] = color;
+            row++;
         }
+        while (--row > r)
+            currentBoard.getColor()[row][c] = color;
     }
 
     private void flipDownLeft(byte r, byte c, boolean color) {
-        for (byte row = (byte) (r+1); row < currentBoard.getColor().length; row++) {
-            c--;
-            if (c < 0 || shouldStopUpdatingDirection(row,c, color))
+        byte row = (byte) (r+1);
+        byte col = (byte) (c-1);
+        while (currentBoard.getColor()[row][col] == color) {
+            if (row >= currentBoard.getColor().length || col < 0 || !currentBoard.getHasTile()[row][col])
                 return;
-            currentBoard.getColor()[row][c] = color;
+            row++;
+            col--;
         }
+        while (--row > r && ++col < c)
+            currentBoard.getColor()[row][col] = color;
     }
 
     private void flipLeft(byte r, byte c, boolean color) {
-        for (byte col = (byte) (c - 1); col >= 0; col--) {
-            if (shouldStopUpdatingDirection(r, col, color))
+        byte col = (byte) (c-1);
+        while (currentBoard.getColor()[r][col] == color) {
+            if (col < 0 || !currentBoard.getHasTile()[r][col])
                 return;
-            currentBoard.getColor()[r][col] = color;
+            col--;
         }
+        while (++col < c)
+            currentBoard.getColor()[r][col] = color;
     }
 
     private void flipUpLeft(byte r, byte c, boolean color) {
-        for (byte row = (byte) (r-1); row >= 0; row--) {
-            c--;
-            if (c < 0 || shouldStopUpdatingDirection(row,c, color))
+        byte row = (byte) (r-1);
+        byte col = (byte) (c-1);
+        while (currentBoard.getColor()[row][col] == color) {
+            if (row < 0 || col < 0 || !currentBoard.getHasTile()[row][col])
                 return;
-            currentBoard.getColor()[row][c] = color;
+            row--;
+            col--;
         }
+        while (++row < r && ++col < c)
+            currentBoard.getColor()[row][col] = color;
     }
 
     /**
